3dd9749629eb0884f26ae9aa889f0ebd6d9143b8
bm05209 <bm05209@georgiasouthern.edu>
2016-11-06 19:04:48 -0500

    Changed stuff to greeting display

--- SalesTool/src/Main.java
+++ SalesTool/src/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		data.display();
 	}
 	private static void displayGreeting(){
-		System.out.println("Hello Happy Sales People!");
-		System.out.println("This App Shows Sales Data");
+		System.out.println("Hello Happy Sales People! I didnt make it caps before so I added this");
+		System.out.println("This App Shows Sales Data. Same same same same same");
 		
 		System.out.println("Test1");
 
